0892867c91c65ef5bc807710fe973ff9a060eca3
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2026-04-13 23:39:06 +0500

    123

---
+++ Tyuiu.KlochenokVA.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KlochenokVA.Sprint1.Task4.V5.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculateValid()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 4;
+            double wait = 0.25;
+            Assert.AreEqual(wait, ds.Calculate(x, y));
+        }
+    }
+}
--- Tyuiu.KlochenokVA.Sprint1.Task4.V5/Program.cs
+++ Tyuiu.KlochenokVA.Sprint1.Task4.V5/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.KlochenokVA.Sprint1.Task3.V13.Lib;
+using Tyuiu.KlochenokVA.Sprint1.Task4.V5.Lib;
 
-namespace Tyuiu.KlochenokVA.Sprint1.Task3.V13
+namespace Tyuiu.KlochenokVA.Sprint1.Task4.V5
 {
     class Program
     {
@@ -16,28 +16,30 @@ namespace Tyuiu.KlochenokVA.Sprint1.Task3.V13
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
-            Console.WriteLine("* Задание #3                                                              *");
-            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
             Console.WriteLine("* Выполнил: Клоченок Владимир Алексеевич | ИСПб-25-1                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая объявляет необходимые переменные, выполняет *");
-            Console.WriteLine("* указанные расчёты и печатает результат на экране.                       *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
             Console.WriteLine("***************************************************************************");
 
-            int numbers = 222;
-            int number = 2;
-            Console.WriteLine("Заданое трехзначное число = " + numbers);
+            double x, y;
+            Console.WriteLine("* Введите X:                                                             *");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите Y:                                                             *");
+            y = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
-            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
 
-            Console.WriteLine("Произведение цифр заданого числа = " + ds.MultiplyOfDigits(number));
-            Console.ReadKey();
+            Console.WriteLine("Ответ: " + ds.Calculate(x, y));
+            Console.ReadLine();
 
 
 
