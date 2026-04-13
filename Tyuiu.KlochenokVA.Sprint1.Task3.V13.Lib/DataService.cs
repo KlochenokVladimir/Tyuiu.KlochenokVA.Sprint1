629aac539892090a500dffe5d756cc455f6ea519
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2026-04-13 23:28:48 +0500

    2

--- Tyuiu.KlochenokVA.Sprint1.Task3.V13.Lib/DataService.cs
+++ Tyuiu.KlochenokVA.Sprint1.Task3.V13.Lib/DataService.cs
@@ -9,9 +9,16 @@ namespace Tyuiu.KlochenokVA.Sprint1.Task3.V13.Lib
 {
     public class DataService : ISprint1Task3V13
     {
-        public double MultiplyOfDigits(double number)
+        public double MultiplyOfDigits(double x)
         {
-            return number * number * number;
+            int y = (int)x;
+            double digit1 = y / 100;
+            double digit2 = (y / 10) % 10;
+            double digit3 = y % 10;
+
+            double product = digit1 * digit2 * digit3;
+            return Math.Round(product, 3);
+
         }
     }
 }
