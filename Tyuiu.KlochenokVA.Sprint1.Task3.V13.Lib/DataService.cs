ff9cef701d02c3138cee2e005e845a6d1c1d8bcb
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2026-04-13 23:27:35 +0500

    2

---
+++ Tyuiu.KlochenokVA.Sprint1.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KlochenokVA.Sprint1.Task3.V13.Lib
+{
+    public class DataService : ISprint1Task3V13
+    {
+        public double MultiplyOfDigits(double number)
+        {
+            return number * number * number;
+        }
+    }
+}
---
+++ Tyuiu.KlochenokVA.Sprint1.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KlochenokVA.Sprint1.Task3.V13.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint1.Task3.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double number = 2;
+
+            double wait = 8;
+            var res = ds.MultiplyOfDigits(number);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
